cc80069c44160e898c0d563d159c38316b142988
chouheiwa <849131492@qq.com>
2024-08-04 10:52:12 +0800

    Update run code.

--- run_test.sh
+++ run_test.sh
@@ -7,7 +7,8 @@ all_data_array=("bad")
 for model_script in "${all_models_script[@]}"; do
     for data_type in "${all_data_array[@]}"; do
         dataset_name="BUSI_${data_type}"
+        dataset_path="/home/chouheiwa/machine_learning/dataset/BUSI数据集/${dataset_name}"
         echo "Running ${model_script} with ${dataset_name} data"
-        example_script/${model_script} "${dataset_name}"
+        example_script/${model_script} "${dataset_name}" "${dataset_path}"
     done
 done
--- run_train.sh
+++ run_train.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-all_models_script=("malunet_train.sh")
+all_models_script=("unet_train.sh" "ours/conv_net_train.sh")
 all_data_array=("all" "bad" "benign" "malignant")
 #all_data_array=("bad")
 #all_data_array=("malignant")
 
 for model_script in "${all_models_script[@]}"; do
     for data_type in "${all_data_array[@]}"; do
+        dataset_name="BUSI_${data_type}"
+        dataset_path="/home/chouheiwa/machine_learning/dataset/BUSI数据集/${dataset_name}"
         echo "Running ${model_script} with BUSI_${data_type} data"
-        example_script/${model_script} "BUSI_${data_type}" "--need_early_stop True"
+        example_script/${model_script} "BUSI_${data_type}" "${dataset_path}"
     done
 done
